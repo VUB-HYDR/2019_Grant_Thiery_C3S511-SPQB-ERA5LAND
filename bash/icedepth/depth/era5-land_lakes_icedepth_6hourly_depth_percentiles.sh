3137df2f6fde14fff575f87c4bbcbfc8fe9297c6
lwilgrant <44237689+lwilgrant@users.noreply.github.com>
2020-04-02 14:48:11 +0200

    Add files via upload

--- bash/icedepth/depth/era5-land_lakes_icedepth_6hourly_depth_percentiles.sh
+++ bash/icedepth/depth/era5-land_lakes_icedepth_6hourly_depth_percentiles.sh
@@ -13,14 +13,11 @@
 # INITIALIZATION
 # =======================================================================
 
-# load CDO
-module load CDO
-
 # set output directory
 outDIR=/theia/data/brussel/101/vsc10116/C3S_511/era5-land/icedepth/depth
 
 # user scratch directory
-scratchDIR=/theia/scratch/projects/climate/users/lgrant
+scratchDIR=/theia/scratch/projects/climate/users/lgrant/era5-land/proc/icedepth_perc
 
 # set mask directory (lakecover)
 maskDIR=/theia/scratch/projects/climate/data/dataset/era5-land/lakes/lakecover
@@ -29,7 +26,7 @@ maskDIR=/theia/scratch/projects/climate/data/dataset/era5-land/lakes/lakecover
 inDIR=/theia/scratch/projects/climate/data/dataset/era5-land/lakes/icedepth
 
 # years
-YEARs=('2001_2005' '2006_2007' '2008_2009' '2010_2011' '2012_2013' '2014_2015' '2016_2017' '2018_2019')
+YEARs=('1981_1982' '1983_1984' '1985_1986' '1987_1988' '1989_1990' '1991_1992' '1993_1994' '1995_1996' '1997_1998' '1999_2000' '2001_2005' '2006_2007' '2008_2009' '2010_2011' '2012_2013' '2014_2015' '2016_2017' '2018_2019')
 
 # percentiles
 PERCENTs=('1' '5' '10' '50' '90' '95' '99')
@@ -56,18 +53,18 @@ echo ' '
 # prep start file to day res
 for YEAR in "${YEARs[@]}"; do
 
-    cdo -b F64 -O -L setreftime,2001-01-01,00:00:00,1days -settaxis,$((${YEAR:0:4}+0))-01-01,00:00:00,1days -daymean era5-land_lakes_icedepth_6hourly_${YEAR}.nc $scratchDIR/startfile_${YEAR}.nc
+    cdo -b F64 -O -L setreftime,1981-01-01,00:00:00,1days -settaxis,$((${YEAR:0:4}+0))-01-01,00:00:00,1days -daymean era5-land_lakes_icedepth_6hourly_${YEAR}.nc $scratchDIR/startfile_${YEAR}.nc
 
 done
 
-cdo -b F64 mergetime $scratchDIR/startfile_*.nc $scratchDIR/icedepth_2001_2019_unmasked.nc
+cdo -b F64 mergetime $scratchDIR/startfile_*.nc $scratchDIR/icedepth_1981_2018_unmasked.nc
 
 rm $scratchDIR/startfile_*.nc
 
 # mask starting file
-cdo ifthen $maskDIR/era5-land_lakemask.nc $scratchDIR/icedepth_2001_2019_unmasked.nc $scratchDIR/icedepth_daily_2001_2019.nc
+cdo ifthen $maskDIR/era5-land_lakemask.nc $scratchDIR/icedepth_1981_2018_unmasked.nc $scratchDIR/icedepth_daily_1981_2018.nc
 
-rm $scratchDIR/icedepth_2001_2019_unmasked.nc
+rm $scratchDIR/icedepth_1981_2018_unmasked.nc
 
 # ==============================================================================
 # PERCENTILE BOUNDS
@@ -79,10 +76,10 @@ echo 'BOUNDS CALC'
 echo ' '
 
 
-cdo timmin $scratchDIR/icedepth_daily_2001_2019.nc $scratchDIR/minfile.nc
+cdo timmin $scratchDIR/icedepth_daily_1981_2018.nc $scratchDIR/minfile.nc
 
 
-cdo timmax $scratchDIR/icedepth_daily_2001_2019.nc $scratchDIR/maxfile.nc
+cdo timmax $scratchDIR/icedepth_daily_1981_2018.nc $scratchDIR/maxfile.nc
 
 # ==============================================================================
 # PERCENTILES
@@ -96,15 +93,15 @@ echo ' '
 
 for PERC in "${PERCENTs[@]}"; do
 
-    cdo -b F64 -O -L timpctl,$((${PERC}+0)) $scratchDIR/icedepth_daily_2001_2019.nc $scratchDIR/minfile.nc $scratchDIR/maxfile.nc $outDIR/percentiles/era5-land_lakes_icedepth_percentile_${PERC}.nc
+    cdo -b F64 -O -L timpctl,$((${PERC}+0)) $scratchDIR/icedepth_daily_1981_2018.nc $scratchDIR/minfile.nc $scratchDIR/maxfile.nc $outDIR/percentiles/era5-land_lakes_icedepth_percentile_${PERC}_1981_2018.nc
 
-    cdo -b F64 -O -L fldmean $outDIR/percentiles/era5-land_lakes_icedepth_percentile_${PERC}.nc $outDIR/percentiles/era5-land_lakes_icedepth_percentile_${PERC}_fldmean.nc
+    cdo -b F64 -O -L fldmean $outDIR/percentiles/era5-land_lakes_icedepth_percentile_${PERC}.nc $outDIR/percentiles/era5-land_lakes_icedepth_percentile_${PERC}_fldmean_1981_2018.nc
 
-    cdo -b F64 -O -L fldstd $outDIR/percentiles/era5-land_lakes_icedepth_percentile_${PERC}.nc $outDIR/percentiles/era5-land_lakes_icedepth_percentile_${PERC}_fldstd.nc
+    cdo -b F64 -O -L fldstd $outDIR/percentiles/era5-land_lakes_icedepth_percentile_${PERC}.nc $outDIR/percentiles/era5-land_lakes_icedepth_percentile_${PERC}_fldstd_1981_2018.nc
 
 done
 
 
-rm $scratchDIR/icedepth_daily_2001_2019.nc
+rm $scratchDIR/icedepth_daily_1981_2018.nc
 rm $scratchDIR/minfile.nc
 rm $scratchDIR/maxfile.nc
--- bash/icedepth/depth/era5-land_lakes_icedepth_6hourly_depth_proc.sh
+++ bash/icedepth/depth/era5-land_lakes_icedepth_6hourly_depth_proc.sh
@@ -13,14 +13,11 @@
 # INITIALIZATION
 # =======================================================================
 
-# load CDO
-module load CDO
-
 # set output directory
 outDIR=/theia/data/brussel/101/vsc10116/C3S_511/era5-land/icedepth/depth
 
 # user scratch directory
-scratchDIR=/theia/scratch/projects/climate/users/lgrant
+scratchDIR=/theia/scratch/projects/climate/users/lgrant/era5-land/proc/icedepth
 
 # set mask directory (lakecover)
 maskDIR=/theia/scratch/projects/climate/data/dataset/era5-land/lakes/lakecover
@@ -32,7 +29,7 @@ inDIR=/theia/scratch/projects/climate/data/dataset/era5-land/lakes/icedepth
 MONTHs=('JAN' 'FEB' 'MAR' 'APR' 'MAY' 'JUN' 'JUL' 'AUG' 'SEP' 'OCT' 'NOV' 'DEC')
 
 # years
-YEARs=('2001_2005' '2006_2007' '2008_2009' '2010_2011' '2012_2013' '2014_2015' '2016_2017' '2018_2019')
+YEARs=('1981_1982' '1983_1984' '1985_1986' '1987_1988' '1989_1990' '1991_1992' '1993_1994' '1995_1996' '1997_1998' '1999_2000' '2001_2005' '2006_2007' '2008_2009' '2010_2011' '2012_2013' '2014_2015' '2016_2017' '2018_2019')
 
 # ==============================================================================
 # PROCESSING
@@ -58,18 +55,18 @@ echo ' '
 # prep start file to day res
 for YEAR in "${YEARs[@]}"; do
 
-    cdo -b F64 -O -L setreftime,2001-01-01,00:00:00,1months -settaxis,$((${YEAR:0:4}+0))-01-01,00:00:00,1months -monmean era5-land_lakes_icedepth_6hourly_${YEAR}.nc $scratchDIR/depth/startfile_${YEAR}.nc
+    cdo -b F64 -O -L setreftime,1981-01-01,00:00:00,1months -settaxis,$((${YEAR:0:4}+0))-01-01,00:00:00,1months -monmean era5-land_lakes_icedepth_6hourly_${YEAR}.nc $scratchDIR/startfile_${YEAR}.nc
 
 done
 
-cdo -b F64 mergetime $scratchDIR/depth/startfile_*.nc $scratchDIR/depth/icedepth_2001_2019_unmasked.nc
+cdo -b F64 mergetime $scratchDIR/startfile_*.nc $scratchDIR/icedepth_1981_2018_unmasked.nc
 
-rm $scratchDIR/depth/startfile_*.nc
+rm $scratchDIR/startfile_*.nc
 
 # mask starting file
-cdo ifthen $maskDIR/era5-land_lakemask.nc $scratchDIR/depth/icedepth_2001_2019_unmasked.nc $scratchDIR/depth/icedepth_monthly_2001_2019.nc
+cdo ifthen $maskDIR/era5-land_lakemask.nc $scratchDIR/icedepth_1981_2018_unmasked.nc $scratchDIR/icedepth_monthly_1981_2018.nc
 
-rm $scratchDIR/depth/icedepth_2001_2019_unmasked.nc
+rm $scratchDIR/icedepth_1981_2018_unmasked.nc
 
 # ==============================================================================
 # TIMMEANS & FLDMEANS
@@ -83,7 +80,7 @@ echo ' '
 
 for i in $(seq 0 11); do
 
-    cdo -b F64 -O -L timmean -selmon,$(($i+1)) -seldate,2001-01-01T00:00:00,2018-12-31T00:00:00 $scratchDIR/depth/icedepth_monthly_2001_2019.nc $outDIR/timmean/era5-land_lakes_icedepth_timmean_${MONTHs[$i]}_2001_2018.nc
+    cdo -b F64 -O -L timmean -selmon,$(($i+1)) -seldate,1981-01-01T00:00:00,2018-12-31T00:00:00 $scratchDIR/icedepth_monthly_1981_2018.nc $outDIR/timmean/era5-land_lakes_icedepth_timmean_${MONTHs[$i]}_1981_2018.nc
 
 done
 
@@ -96,7 +93,7 @@ echo ' '
 
 for i in $(seq 0 11); do
 
-    cdo -b F64 -O -L fldmean -selmon,$(($i+1)) -seldate,2001-01-01T00:00:00,2018-12-31T00:00:00 $scratchDIR/depth/icedepth_monthly_2001_2019.nc $outDIR/fldmean/era5-land_lakes_icedepth_fldmean_${MONTHs[$i]}_2001_2018.nc
+    cdo -b F64 -O -L fldmean -selmon,$(($i+1)) -seldate,1981-01-01T00:00:00,2018-12-31T00:00:00 $scratchDIR/icedepth_monthly_1981_2018.nc $outDIR/fldmean/era5-land_lakes_icedepth_fldmean_${MONTHs[$i]}_1981_2018.nc
 
 done
 
@@ -113,16 +110,16 @@ echo ' '
 for i in $(seq 0 11); do
 
     # signal (first 5 years)
-    cdo -b F64 -O -L timmean -seldate,2001-01-01T00:00:00,2005-12-31T00:00:00 -selmon,$(($i+1)) $scratchDIR/depth/icedepth_monthly_2001_2019.nc $scratchDIR/depth/era5-land_lakes_icedepth_${MONTHs[$i]}_2001_2005_5year.nc
+    cdo -b F64 -O -L timmean -seldate,1981-01-01T00:00:00,1990-12-31T00:00:00 -selmon,$(($i+1)) $scratchDIR/icedepth_monthly_1981_2018.nc $scratchDIR/era5-land_lakes_icedepth_${MONTHs[$i]}_1981_1990_10year.nc
 
     # signal (last 5 years)
-    cdo -b F64 -O -L timmean -seldate,2014-01-01T00:00:00,2018-12-31T00:00:00 -selmon,$(($i+1)) $scratchDIR/depth/icedepth_monthly_2001_2019.nc $scratchDIR/depth/era5-land_lakes_icedepth_${MONTHs[$i]}_2014_2018_5year.nc
+    cdo -b F64 -O -L timmean -seldate,2009-01-01T00:00:00,2018-12-31T00:00:00 -selmon,$(($i+1)) $scratchDIR/icedepth_monthly_1981_2018.nc $scratchDIR/era5-land_lakes_icedepth_${MONTHs[$i]}_2009_2018_10year.nc
 
     #signal (diff)
-    cdo -b F64 -O -L sub $scratchDIR/depth/era5-land_lakes_icedepth_${MONTHs[$i]}_2014_2018_5year.nc $scratchDIR/depth/era5-land_lakes_icedepth_${MONTHs[$i]}_2001_2005_5year.nc $outDIR/signals/era5-land_lakes_icedepth_signal_${MONTHs[$i]}_2001_2019.nc
+    cdo -b F64 -O -L sub $scratchDIR/era5-land_lakes_icedepth_${MONTHs[$i]}_2009_2018_10year.nc $scratchDIR/era5-land_lakes_icedepth_${MONTHs[$i]}_1981_1990_10year.nc $outDIR/signals/era5-land_lakes_icedepth_signal_${MONTHs[$i]}_1981_2018.nc
 
 done
 
-rm $scratchDIR/depth/era5-land_lakes*.nc
+rm $scratchDIR/era5-land_lakes*.nc
 
-rm $scratchDIR/depth/icedepth_monthly_2001_2019.nc
+rm $scratchDIR/icedepth_monthly_1981_2018.nc
