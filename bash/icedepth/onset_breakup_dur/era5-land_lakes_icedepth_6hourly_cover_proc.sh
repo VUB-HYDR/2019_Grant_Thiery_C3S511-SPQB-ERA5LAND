442abbaa766ef9178b461b1593f64deb245605ef
lwilgrant <44237689+lwilgrant@users.noreply.github.com>
2020-05-04 07:56:15 +0200

    Update era5-land_lakes_icedepth_6hourly_cover_proc.sh

--- bash/icedepth/onset_breakup_dur/era5-land_lakes_icedepth_6hourly_cover_proc.sh
+++ bash/icedepth/onset_breakup_dur/era5-land_lakes_icedepth_6hourly_cover_proc.sh
@@ -57,14 +57,14 @@ for YEAR in "${YEARs[@]}"; do
 
 done
 
-cdo -b F64 mergetime $scratchDIR/startfile_*.nc $scratchDIR/icecover_1981_2018_unmasked.nc
+cdo -b F64 mergetime $scratchDIR/startfile_*.nc $scratchDIR/icecover_1981_2019_unmasked.nc
 
 rm $scratchDIR/startfile_*.nc
 
 # mask starting file
-cdo ifthen $maskDIR/era5-land_lakemask.nc $scratchDIR/icecover_1981_2018_unmasked.nc $scratchDIR/icecover_daily_1981_2018.nc
+cdo ifthen $maskDIR/era5-land_lakemask.nc $scratchDIR/icecover_1981_2019_unmasked.nc $scratchDIR/icecover_daily_1981_2019.nc
 
-rm $scratchDIR/icecover_1981_2018_unmasked.nc
+rm $scratchDIR/icecover_1981_2019_unmasked.nc
 
 # ==============================================================================
 # ICE START
@@ -77,18 +77,18 @@ echo ' '
 
 
 # select October to December for 1st to 2nd-last year
-cdo -b F64 -O -L setctomiss,0 -muldoy -selmon,10/12 -seldate,1981-01-01T00:00:00,2018-01-01T00:00:00 $scratchDIR/icecover_daily_1981_2018.nc $scratchDIR/icecover_daily_1981_2018_part1.nc
+cdo -b F64 -O -L setctomiss,0 -muldoy -selmon,10/12 -seldate,1981-01-01T00:00:00,2019-01-01T00:00:00 $scratchDIR/icecover_daily_1981_2019.nc $scratchDIR/icecover_daily_1981_2019_part1.nc
 
 
 # select January to September for 2nd to last year (lag by 365)
-cdo -b F64 -O -L addc,365 -setctomiss,0 -muldoy -selmon,1/9 -seldate,1982-01-01T00:00:00,2018-12-31T00:00:00 $scratchDIR/icecover_daily_1981_2018.nc $scratchDIR/icecover_daily_1981_2018_part2.nc
+cdo -b F64 -O -L addc,365 -setctomiss,0 -muldoy -selmon,1/9 -seldate,1982-01-01T00:00:00,2019-12-31T00:00:00 $scratchDIR/icecover_daily_1981_2019.nc $scratchDIR/icecover_daily_1981_2019_part2.nc
 
 
 # merge selections
-cdo -b F64 mergetime $scratchDIR/icecover_daily_1981_2018_part1.nc $scratchDIR/icecover_daily_1981_2018_part2.nc $scratchDIR/dummy_final.nc
+cdo -b F64 mergetime $scratchDIR/icecover_daily_1981_2019_part1.nc $scratchDIR/icecover_daily_1981_2019_part2.nc $scratchDIR/dummy_final.nc
 
 
-rm $scratchDIR/icecover_daily_1981_2018_part*.nc
+rm $scratchDIR/icecover_daily_1981_2019_part*.nc
 
 
 for i in $(seq 1981 2017); do
@@ -111,21 +111,21 @@ rm $scratchDIR/dummy_start_*.nc
 cdo -b F64 -O -L setreftime,1981-01-01,00:00:00,1years -settaxis,1981-01-01,00:00:00,1years -setattribute,icestart@long_name='First day of lake ice cover' -setname,'icestart' -setunit,"day of hydrological year" $scratchDIR/icecover_start_1981_2018_dummy.nc $outDIR/era5-land_lakes_icecover_start_1981_2018.nc
 
 
-# signal (first 5 years)
-cdo -b F64 -O -L timmean -seldate,1981-01-01T00:00:00,1990-12-31T00:00:00 $outDIR/era5-land_lakes_icecover_start_1981_2018.nc $scratchDIR/era5-land_lakes_icecover_start_1981_1990_10year.nc
+# signal (first 10 years)
+cdo -b F64 -O -L timmean -seldate,1981-01-01T00:00:00,1990-12-31T00:00:00 $outDIR/era5-land_lakes_icecover_start_1981_2019.nc $scratchDIR/era5-land_lakes_icecover_start_1981_1990_10year.nc
 
 
-# signal (last 5 years)
-cdo -b F64 -O -L timmean -seldate,2009-01-01T00:00:00,2018-12-31T00:00:00 $outDIR/era5-land_lakes_icecover_start_1981_2018.nc $scratchDIR/era5-land_lakes_icecover_start_2009_2018_10year.nc
+# signal (last 10 years)
+cdo -b F64 -O -L timmean -seldate,2010-01-01T00:00:00,2019-12-31T00:00:00 $outDIR/era5-land_lakes_icecover_start_1981_2019.nc $scratchDIR/era5-land_lakes_icecover_start_2010_2019_10year.nc
 
 
 #signal (diff)
-cdo -b F64 -O -L sub $scratchDIR/era5-land_lakes_icecover_start_2009_2018_10year.nc $scratchDIR/era5-land_lakes_icecover_start_1981_1990_10year.nc $outDIR/signals/era5-land_lakes_icecover_start_signals_1981_2018.nc
+cdo -b F64 -O -L sub $scratchDIR/era5-land_lakes_icecover_start_2010_2019_10year.nc $scratchDIR/era5-land_lakes_icecover_start_1981_1990_10year.nc $outDIR/signals/era5-land_lakes_icecover_start_signals_1981_2019.nc
 
 
 rm $scratchDIR/era5-land_lakes_icecover_start_1981_1990_10year.nc
-rm $scratchDIR/era5-land_lakes_icecover_start_2009_2018_10year.nc
-rm $scratchDIR/icecover_start_1981_2018_dummy.nc
+rm $scratchDIR/era5-land_lakes_icecover_start_2010_2019_10year.nc
+rm $scratchDIR/icecover_start_1981_2019_dummy.nc
 
 # ==============================================================================
 # ICE END
@@ -138,18 +138,18 @@ echo ' '
 
 
 # select September to December for 1st to 2nd-last year
-cdo -b F64 -O -L setctomiss,0 -muldoy -selmon,9/12 -seldate,1981-01-01T00:00:00,2018-01-01T00:00:00 $scratchDIR/icecover_daily_1981_2018.nc $scratchDIR/icecover_daily_1981_2018_part1.nc
+cdo -b F64 -O -L setctomiss,0 -muldoy -selmon,9/12 -seldate,1981-01-01T00:00:00,2019-01-01T00:00:00 $scratchDIR/icecover_daily_1981_2019.nc $scratchDIR/icecover_daily_1981_2019_part1.nc
 
 
 # select January to August for 2nd to last year (lag by 365)
-cdo -b F64 -O -L addc,365 -setctomiss,0 -muldoy -selmon,1/8 -seldate,1982-01-01T00:00:00,2018-12-31T00:00:00 $scratchDIR/icecover_daily_1981_2018.nc $scratchDIR/icecover_daily_1981_2018_part2.nc
+cdo -b F64 -O -L addc,365 -setctomiss,0 -muldoy -selmon,1/8 -seldate,1982-01-01T00:00:00,2019-12-31T00:00:00 $scratchDIR/icecover_daily_1981_2019.nc $scratchDIR/icecover_daily_1981_2019_part2.nc
 
 
 # merge selections
-cdo -b F64 mergetime $scratchDIR/icecover_daily_1981_2018_part1.nc $scratchDIR/icecover_daily_1981_2018_part2.nc $scratchDIR/dummy_final.nc
+cdo -b F64 mergetime $scratchDIR/icecover_daily_1981_2019_part1.nc $scratchDIR/icecover_daily_1981_2019_part2.nc $scratchDIR/dummy_final.nc
 
 
-rm $scratchDIR/icecover_daily_1981_2018_part*.nc
+rm $scratchDIR/icecover_daily_1981_2019_part*.nc
 
 
 for i in $(seq 1981 2017); do
@@ -163,7 +163,7 @@ done
 rm $scratchDIR/dummy_final.nc
 
 
-cdo -b F64 -O mergetime $scratchDIR/dummy_end_*.nc $scratchDIR/icecover_end_1981_2018_dummy.nc
+cdo -b F64 -O mergetime $scratchDIR/dummy_end_*.nc $scratchDIR/icecover_end_1981_2019_dummy.nc
 
 
 rm $scratchDIR/dummy_end_*.nc
@@ -173,19 +173,19 @@ cdo -b F64 -O -L setreftime,1981-01-01,00:00:00,1years -settaxis,1981-01-01,00:0
 
 
 # signal (first 5 years)
-cdo -b F64 -O -L timmean -seldate,1981-01-01T00:00:00,1990-12-31T00:00:00 $outDIR/era5-land_lakes_icecover_end_1981_2018.nc $scratchDIR/era5-land_lakes_icecover_end_1981_1990_10year.nc
+cdo -b F64 -O -L timmean -seldate,1981-01-01T00:00:00,1990-12-31T00:00:00 $outDIR/era5-land_lakes_icecover_end_1981_2019.nc $scratchDIR/era5-land_lakes_icecover_end_1981_1990_10year.nc
 
 
 # signal (last 5 years)
-cdo -b F64 -O -L timmean -seldate,2009-01-01T00:00:00,2018-12-31T00:00:00 $outDIR/era5-land_lakes_icecover_end_1981_2018.nc $scratchDIR/era5-land_lakes_icecover_end_2009_2018_10year.nc
+cdo -b F64 -O -L timmean -seldate,2010-01-01T00:00:00,2019-12-31T00:00:00 $outDIR/era5-land_lakes_icecover_end_1981_2019.nc $scratchDIR/era5-land_lakes_icecover_end_2010_2019_10year.nc
 
 
 #signal (diff)
-cdo -b F64 -O -L sub $scratchDIR/era5-land_lakes_icecover_end_2009_2018_10year.nc $scratchDIR/era5-land_lakes_icecover_end_1981_1990_10year.nc $outDIR/signals/era5-land_lakes_icecover_end_signal_1981_2018.nc
+cdo -b F64 -O -L sub $scratchDIR/era5-land_lakes_icecover_end_2010_2019_10year.nc $scratchDIR/era5-land_lakes_icecover_end_1981_1990_10year.nc $outDIR/signals/era5-land_lakes_icecover_end_signal_1981_2019.nc
 
 rm $scratchDIR/era5-land_lakes_icecover_end_1981_1990_10year.nc
-rm $scratchDIR/era5-land_lakes_icecover_end_2009_2018_10year.nc
-rm $scratchDIR/icecover_end_1981_2018_dummy.nc
+rm $scratchDIR/era5-land_lakes_icecover_end_2010_2019_10year.nc
+rm $scratchDIR/icecover_end_1981_2019_dummy.nc
 
 # ==============================================================================
 # DURATION
@@ -198,15 +198,15 @@ echo 'ICE DURATION CALC'
 echo ' '
 
 
-for i in $(seq 1981 2017); do
+for i in $(seq 1981 2018); do
 
     # ice duration
-    cdo -b F64 -L timsum -seldate,$i-10-01T00:00:00,$(($i+1))-09-31T00:00:00 $scratchDIR/icecover_daily_1981_2018.nc $scratchDIR/dummy_duration_$i.nc
+    cdo -b F64 -L timsum -seldate,$i-10-01T00:00:00,$(($i+1))-09-31T00:00:00 $scratchDIR/icecover_daily_1981_2019.nc $scratchDIR/dummy_duration_$i.nc
 
 done
 
 
-cdo -b F64 mergetime $scratchDIR/dummy_duration_*.nc $scratchDIR/icecover_duration_1981_2018_dummy.nc
+cdo -b F64 mergetime $scratchDIR/dummy_duration_*.nc $scratchDIR/icecover_duration_1981_2019_dummy.nc
 
 
 rm $scratchDIR/dummy_duration_*.nc
@@ -216,53 +216,53 @@ cdo -b F64 -O -L setctomiss,0 -setreftime,1981-01-01,00:00:00,1years -settaxis,1
 
 
 # signal (first 5 years)
-cdo -b F64 -O -L timmean -seldate,1981-01-01T00:00:00,1990-12-31T00:00:00 $outDIR/era5-land_lakes_icecover_duration_1981_2018.nc $scratchDIR/era5-land_lakes_icecover_dur_1981_1990_10year.nc
+cdo -b F64 -O -L timmean -seldate,1981-01-01T00:00:00,1990-12-31T00:00:00 $outDIR/era5-land_lakes_icecover_duration_1981_2019.nc $scratchDIR/era5-land_lakes_icecover_dur_1981_1990_10year.nc
 
 
 # signal (last 5 years)
-cdo -b F64 -O -L timmean -seldate,2009-01-01T00:00:00,2018-12-31T00:00:00 $outDIR/era5-land_lakes_icecover_duration_1981_2018.nc $scratchDIR/era5-land_lakes_icecover_dur_2009_2018_10year.nc
+cdo -b F64 -O -L timmean -seldate,2010-01-01T00:00:00,2019-12-31T00:00:00 $outDIR/era5-land_lakes_icecover_duration_1981_2019.nc $scratchDIR/era5-land_lakes_icecover_dur_2010_2019_10year.nc
 
 
 #signal (diff)
-cdo -b F64 -O -L sub $scratchDIR/era5-land_lakes_icecover_dur_2009_2018_10year.nc $scratchDIR/era5-land_lakes_icecover_dur_1981_1990_10year.nc $outDIR/signals/era5-land_lakes_icecover_dur_signal_1981_2018.nc
+cdo -b F64 -O -L sub $scratchDIR/era5-land_lakes_icecover_dur_2009_2019_10year.nc $scratchDIR/era5-land_lakes_icecover_dur_1981_1990_10year.nc $outDIR/signals/era5-land_lakes_icecover_dur_signal_1981_2019.nc
 
 
 rm $scratchDIR/era5-land_lakes_icecover_dur_1981_1990_10year.nc
-rm $scratchDIR/era5-land_lakes_icecover_dur_2009_2018_10year.nc
-rm $scratchDIR/icecover_duration_1981_2018_dummy.nc
+rm $scratchDIR/era5-land_lakes_icecover_dur_2010_2019_10year.nc
+rm $scratchDIR/icecover_duration_1981_2019_dummy.nc
 
 # ==============================================================================
 # GLOBAL MEANS
 # ==============================================================================
 
 #ice start fldmeans
-cdo -b F64 fldmean $outDIR/era5-land_lakes_icecover_start_1981_2018.nc $outDIR/fldmean/era5-land_lakes_icecover_start_global_fldmean_1981_2018.nc
+cdo -b F64 fldmean $outDIR/era5-land_lakes_icecover_start_1981_2019.nc $outDIR/fldmean/era5-land_lakes_icecover_start_global_fldmean_1981_2019.nc
 
 
 #ice end fldmeans
-cdo -b F64 fldmean $outDIR/era5-land_lakes_icecover_end_1981_2018.nc $outDIR/fldmean/era5-land_lakes_icecover_end_global_fldmean_1981_2018.nc
+cdo -b F64 fldmean $outDIR/era5-land_lakes_icecover_end_1981_2019.nc $outDIR/fldmean/era5-land_lakes_icecover_end_global_fldmean_1981_2019.nc
 
 
 #ice duration fldmeans
-cdo -b F64 fldmean $outDIR/era5-land_lakes_icecover_duration_1981_2018.nc $outDIR/fldmean/era5-land_lakes_icecover_duration_global_fldmean_1981_2018.nc
+cdo -b F64 fldmean $outDIR/era5-land_lakes_icecover_duration_1981_2019.nc $outDIR/fldmean/era5-land_lakes_icecover_duration_global_fldmean_1981_2019.nc
 
 # ==============================================================================
 # TEMPORAL MEANS
 # ==============================================================================
 
 #ice start timmeans
-cdo -b F64 timmean $outDIR/era5-land_lakes_icecover_start_1981_2018.nc $outDIR/timmean/era5-land_lakes_icecover_start_global_timmean_1981_2018.nc
+cdo -b F64 timmean $outDIR/era5-land_lakes_icecover_start_1981_2019.nc $outDIR/timmean/era5-land_lakes_icecover_start_global_timmean_1981_2019.nc
 
 
 #ice end timmeans
-cdo -b F64 timmean $outDIR/era5-land_lakes_icecover_end_1981_2018.nc $outDIR/timmean/era5-land_lakes_icecover_end_global_timmean_1981_2018.nc
+cdo -b F64 timmean $outDIR/era5-land_lakes_icecover_end_1981_2019.nc $outDIR/timmean/era5-land_lakes_icecover_end_global_timmean_1981_2019.nc
 
 
 #ice duration timmeans
-cdo -b F64 timmean $outDIR/era5-land_lakes_icecover_duration_1981_2018.nc $outDIR/timmean/era5-land_lakes_icecover_duration_global_timmean_1981_2018.nc
+cdo -b F64 timmean $outDIR/era5-land_lakes_icecover_duration_1981_2019.nc $outDIR/timmean/era5-land_lakes_icecover_duration_global_timmean_1981_2019.nc
 
 # ==============================================================================
 # CLEANUP
 # ==============================================================================
 
-rm $scratchDIR/icecover_daily_1981_2018.nc
+rm $scratchDIR/icecover_daily_1981_2019.nc
